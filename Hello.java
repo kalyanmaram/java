ae3fdc8ab481ba5b134153e59830ea1a18c8d5ae
kalyanmaram <saikalyan.maram@unh.edu>
2024-01-24 21:57:57 -0500

    java code 2

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ import java.util.*;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
